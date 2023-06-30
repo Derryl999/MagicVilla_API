0cf9a094eb774465f1feea49e3cbf02341291eca
Derryl999 <derryl.aranha999@gmail.com>
2023-06-30 09:43:53 +0530

    Created Get APIs

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    //[Route("api/VillaAPI")]
+    [Route("api/[controller]")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public List<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.Find(u => u.Id == id);
+        }
+    }
+}
